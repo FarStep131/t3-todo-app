1e782d462d4033ab8e3d4e75114795bf00aa76fa
NaokiYazawa <yazawa.naoki.01@gmail.com>
2023-03-12 20:36:50 +0900

    create Todo component

--- src/server/types.ts
+++ src/server/types.ts
@@ -1,4 +1,10 @@
 import { z } from "zod";
+import type { inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "~/server/api/root";
+
+type RouterOutput = inferRouterOutputs<AppRouter>;
+type allTodosOutput = RouterOutput["todo"]["all"];
+export type Todo = allTodosOutput[number];
 
 export const createInput = z
   .string()
